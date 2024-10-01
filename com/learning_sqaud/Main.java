a7a7c14720f29cc72f65e1ef9709fdfce2bcd53b
SwapnilShewale213 <shewalesk252000@gmail.com>
2024-10-01 22:55:27 +0530

    second commit

---
+++ com/learning_sqaud/Main.java
@@ -0,0 +1,25 @@
+package com.learning_sqaud;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EncapsulationDemo e = new EncapsulationDemo();
+
+        // here method call
+        //System.out.println("Nice to meet you " + e.doSomeOperations());
+
+
+//        e.setFirstName("Sagar");
+//        e.setLastName("Ahire");
+//        e.setRollNo(96);
+//        e.setAddress("California");
+//
+//        System.out.println( "First Name: " + e.getFirstName());
+//        System.out.println("Last Name: " + e.getLastName());
+//        System.out.println("Roll No : " + e.getRollNo());
+//        System.out.println("Address :" + e.getAddress());
+
+
+    }
+}
